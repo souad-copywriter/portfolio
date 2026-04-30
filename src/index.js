d17fa3d5c7cb79b5e6e03ee3476cd80d268ea63b
souad-copywriter <souadabouelkassem@gmail.com>
2026-04-30 16:39:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
